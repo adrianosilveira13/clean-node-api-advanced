89bba0a897b7673bec32bf9fbec710c6c93e9f69
Adriano <kdr@outlook.com.br>
2022-05-11 18:04:01 -0300

    feat: add facebook authentication feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,14 @@
+import { AccessToken } from '@/domain/models'
+import { AuthenticationError } from '@/domain/errors'
+
+export interface FacebookAuthentication {
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
+}
+
+export namespace FacebookAuthentication {
+  export type Params = {
+    params: string
+  }
+
+  export type Result = AccessToken | AuthenticationError
+}
