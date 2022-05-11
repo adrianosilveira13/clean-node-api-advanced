ca570b5ab20c72776713cda06bbf0a629a42f60b
Adriano <kdr@outlook.com.br>
2022-05-11 18:40:36 -0300

    test: ensure Service returns AuthenticationError when LoadFacebookUserApi returns undefined

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,29 +1,34 @@
+import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 
 class FacebookAuthenticationService {
   constructor (private readonly loadFacebookUserApiSpy: LoadFacebookUserApi) {}
 
-  async perform (params: FacebookAuthentication.Params): Promise<void> {
+  async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     await this.loadFacebookUserApiSpy.loadUser(params)
-    return Promise.resolve()
+    return new AuthenticationError()
   }
 }
 
 interface LoadFacebookUserApi {
-  loadUser: (params: LoadFacebookUserApi.Params) => Promise<void>
+  loadUser: (params: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>
 }
 
 namespace LoadFacebookUserApi {
   export type Params = {
     token: string
   }
+
+  export type Result = undefined
 }
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token: string
+  result: undefined
 
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token
+    return this.result
   }
 }
 
@@ -33,5 +38,13 @@ describe('FacebookAuthenticationService', () => {
     const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
     await sut.perform({ token: 'any_token' })
     expect(loadFacebookUserApiSpy.token).toBe('any_token')
+  })
+
+  it('Should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
+    const loadFacebookUserApiSpy = new LoadFacebookUserApiSpy()
+    loadFacebookUserApiSpy.result = undefined
+    const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
+    const authResult = await sut.perform({ token: 'any_token' })
+    expect(authResult).toEqual(new AuthenticationError())
   })
 })
